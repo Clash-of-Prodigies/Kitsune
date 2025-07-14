10df72f0e6f2c9c26aab5631ad439ac36763c22c
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-14 09:42:15 -0500

    Wrap route components in React.StrictMode and MantineProvider for improved performance and styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import { HashRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/home.js';
 import About from './pages/about.js';
 import Unknown from './pages/unknown.js';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import React from 'react';
 
 function App() {
 	return (
 	<HashRouter>
 		<Routes>
-			<Route index path="/" element={<Home />} />
-			<Route path="/about" element={<About />} />
-			<Route path="*" element={<Unknown />} />
+			<Route index path="/" element={
+				<React.StrictMode>
+					<MantineProvider withGlobalStyles withNormalizeCSS>
+						<Home />
+					</MantineProvider>
+				</React.StrictMode>
+			} />
+			<Route path="/about" element={
+				<React.StrictMode>
+					<MantineProvider withGlobalStyles withNormalizeCSS>
+						<About />
+					</MantineProvider>
+				</React.StrictMode>
+				} />
+			<Route path="*" element={
+				<React.StrictMode>
+					<MantineProvider withGlobalStyles withNormalizeCSS>
+						<Unknown />
+					</MantineProvider>
+				</React.StrictMode>
+				} />
 		</Routes>
 	</HashRouter>		
   );
