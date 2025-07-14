96f219f193998c054a64acffd78e2c6524b08693
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-14 09:42:01 -0500

    Add SidebarButton component for icon display with optional badge

---
+++ src/components/SideBarButton.js
@@ -0,0 +1,24 @@
+import { Paper, ActionIcon, Badge } from '@mantine/core';
+
+export default function SidebarButton({ icon, badge }) {
+    return (
+    <Paper
+      shadow="sm"
+      radius="lg"
+      withBorder
+      style={{ position: 'relative', width: 50, height: 50 }}
+    >
+        {badge && (
+            <Badge
+            color="red"
+            variant="filled"
+            size="xs"
+            style={{ position: 'absolute', top: -5, right: -5 }}
+            >{badge}
+            </Badge>
+        )}
+        <ActionIcon variant="transparent" size="xl" style={{ width: '100%', height: '100%' }}>{icon}
+        </ActionIcon>
+    </Paper>
+  );
+}
