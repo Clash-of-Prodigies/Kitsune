1cf1c991ef14a528991a69968c1eeea7593b1f6c
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-25 23:48:02 -0500

    Refactor NewsCard component: enhance article display with new layout, add description and image support

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,12 +1,15 @@
-import { Modal, Stack, Paper, Text, Box, Group, } from '@mantine/core';
+import { Modal, Stack, Paper, Text, Box, Group, Badge, } from '@mantine/core';
 import { IconClockHour4, IconGift, IconNews } from '@tabler/icons-react';
 import { useState } from 'react';
-import Article from './News';
+import Article from './Article';
+import imagi from '../media/dark.png'
 
 const articles = [
     {
         id: 1,
         title: "Floor is Lava",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageUrl: imagi,
         icon: <IconNews size={24} />,
         timeLeft: "7d 17h",
         unread: true,
@@ -39,29 +42,79 @@ const articles = [
         timeLeft: "4d 17h",
         unread: false,
     },
+        {
+        id: 6,
+        title: "Welcome!",
+        icon: <IconGift size={24} />,
+        timeLeft: "4d 17h",
+        unread: false,
+    },
 ];
+
+function NewsArticle({ui = {}, article={}}) {
+    return (
+    <Box key={article.id} onClick={() => ui.ReadArticle(article)} style={{
+        backgroundColor: '#f8fdffff', borderRadius: 10, cursor: 'pointer', padding: 3,
+        display: 'flex', justifyContent: 'space-between', alignItems: 'center',
+        boxShadow: `
+        inset 0 -2px 0 #ffffff,
+        0 2px 4px rgba(0, 0, 0, 0.15)
+        `,
+        }}
+    >
+        <Stack p='md' style={{ backgroundColor: '#c4e1fcff', width: '100%', borderRadius: 10, position: 'relative'}}>
+            <Group spacing="sm">
+                {article.icon}
+                <Text c='#290dddff' style={{ fontFamily: 'sans-serif', fontWeight: 900, }}>{article.title}</Text>
+            </Group>
+            <Group c='white' style={{
+                position: 'absolute', bottom: 3, right: 5,
+                backgroundColor: 'purple', borderRadius: 10, paddingLeft: 5, paddingRight: 5,
+                '--group-gap': '3px',
+            }}
+            >
+                <IconClockHour4 size={16} />
+                <Text size="xs" style={{ fontWeight: 700}}>{article.timeLeft}</Text>
+            </Group>
+            <Badge color="red" variant="filled" size="xs" style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                zIndex: 2,
+                border: '2px solid white',
+                boxShadow: 
+                `0 0 4px #070707ff,
+                inset 0 0 1px #070707ff
+                `,
+            }}
+            display={article.unread ? "default" : "none"}>
+            </Badge>
+        </Stack>
+    </Box>
+    );
+}
 
 function News({ui = {}}) {
     return (
-    <Paper p="md" radius="xl" style={{ backgroundColor: '#d3eaff', width: '100%' }}>
-        <Text align="center" size="xl" weight={700} mb="sm">What's New?</Text>
-        <Stack spacing="sm">
-            {articles.map((article) => (
-                <Box key={article.id} p="xs" onClick={() => ui.ReadArticle(article)} style={{
-                    backgroundColor: '#ecf4ff', borderRadius: 10, cursor: 'pointer',
-                    display: 'flex', justifyContent: 'space-between', alignItems: 'center',
-                }}
-                >
-                    <Group spacing="sm">
-                        {article.icon}
-                        <Text weight={600}>{article.title}</Text>
-                    </Group>
-                    <Group spacing={4}>
-                        <IconClockHour4 size={16} />
-                        <Text size="sm">{article.timeLeft}</Text>
-                    </Group>
-                </Box>
-            ))}
+    <Paper p="xs" radius="xs" style={{ backgroundColor: '#96cbfdff', width: '100%', }}>
+        <Text align="center" c='#f8fdffff' size="xl" mb="sm" style={{
+            fontFamily: 'sans-serif',
+            fontWeight: 900,
+            textShadow:
+            `0px 2px 0px #070707ff,
+            0px 4px 0px #2b2b2cff
+            `,
+        }}>What's New?</Text>
+        <Stack spacing="sm" p="xs" style={{
+            backgroundColor: '#d3eaff',
+            borderRadius: 12,
+			maxHeight: 400,
+			overflowY: 'auto',
+			overflowX: 'hidden',
+			paddingRight: 4,
+		}}
+        >
+            {articles.map((article) => (<NewsArticle ui={ui} article={article} key={article.id}/>))}
         </Stack>
     </Paper>
     );
@@ -78,7 +131,7 @@ export default function NewsCard({ ui={} }) {
     
     return (
     <>
-    <Modal centered withCloseButton={false} radius="xl" padding={0} overlayProps={{ opacity: 0.6 }}
+    <Modal centered withCloseButton={false} radius="lg" padding={0} overlayProps={{ opacity: 0.6 }}
     opened={ui.readNews} onClose={() => ui.ReadNews(false)}
     >
         <News ui={uiState} />
