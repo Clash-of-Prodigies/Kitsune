7c99a59d685cf97f6fe7c1db1928b30cf1fcc88a
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-25 23:47:57 -0500

    Refactor Article component: update layout and styling for improved readability and visual appeal

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,16 +3,24 @@ import { Modal, Text, Paper, Button, Stack, Image } from '@mantine/core';
 export default function Article({ ui = {} }) {
     return (
     <Modal centered withCloseButton={false} radius="xl" padding={0} overlayProps={{ opacity: 0.6 }}
-    opened={ui.article !== null} onClose={() => ui.ReadArticle(null)}
+    opened={ui.article !== null} onClose={() => ui.ReadArticle(null)} size="sm"
     >
-        <Paper p="md" radius="xl" style={{ backgroundColor: '#d3eaff', width: '100%' }}>
-            <Stack spacing="sm">
-                <Text align="center" size="xl" weight={700}>{ui.article?.title ?? 'News Title'}</Text>
-                <Text align="center" size="sm" c="dimmed">{ui.article?.description ?? "Here's the news description."}</Text>
-                <Image radius="md" fit="cover" alt={ui.article?.title}
-                src={ui.article?.imageUrl ?? '/images/default-news.png'}
-                />
-                <Button fullWidth radius="xl" mt="sm">{ui.article?.ctaText ?? 'Play now'}</Button>
+        <Paper p="xs" radius="xs" style={{ backgroundColor: '#96cbfdff', width: '100%', }}>
+            <Text align="center" c='#f8fdffff' size="xl" mb="sm" style={{
+                fontFamily: 'sans-serif',
+                fontWeight: 900,
+                textShadow:
+                    `0px 2px 0px #070707ff,
+                    0px 4px 0px #2b2b2cff
+                    `,
+            }}>{ui.article?.title ?? 'News Title'}</Text>
+
+            <Text align="center" c='#080aa0ff' size="sm" mb="sm" style={{ fontWeight: 900, }}>
+                {ui.article?.description ?? "Here's the news description."}
+            </Text>
+            <Stack spacing="sm" p="xs" style={{ backgroundColor: '#d3eaff', borderRadius: 12, }}>
+                <Image radius="md" fit="contain" alt={ui.article?.title} width={100} height={250} src={ui.article?.imageUrl ?? '/dark.png'} />
+                <Button radius="md" mt="sm" ml="auto" mr="auto" style={{ width: 'fit-content'}}>{ui.article?.ctaText ?? 'Play now'}</Button>
             </Stack>
         </Paper>
     </Modal>
