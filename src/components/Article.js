8a534e7408e7663e1a832ee70d448392fb502fdd
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-27 21:46:27 -0500

    Refactor Article component: clean up code formatting and improve image fallback handling

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,24 +2,19 @@ import { Modal, Text, Paper, Button, Stack, Image } from '@mantine/core';
 
 export default function Article({ ui = {} }) {
     return (
-    <Modal centered withCloseButton={false} radius="xl" padding={0} overlayProps={{ opacity: 0.6 }}
-    opened={ui.article !== null} onClose={() => ui.ReadArticle(null)} size="sm"
+    <Modal centered withCloseButton={false} radius="xl" padding={0} overlayProps={{ opacity: 0.6 }} size="sm"
+    opened={ui.article !== null} onClose={() => ui.ReadArticle(null)}
     >
         <Paper p="xs" radius="xs" style={{ backgroundColor: '#96cbfdff', width: '100%', }}>
-            <Text align="center" c='#f8fdffff' size="xl" mb="sm" style={{
-                fontFamily: 'sans-serif',
-                fontWeight: 900,
-                textShadow:
-                    `0px 2px 0px #070707ff,
-                    0px 4px 0px #2b2b2cff
-                    `,
+            <Text align="center" c='#f8fdffff' size="xl" mb="sm" ff='sans-serif' fw={900} style={{
+                textShadow: '1px 1px #3772ff',
             }}>{ui.article?.title ?? 'News Title'}</Text>
 
             <Text align="center" c='#080aa0ff' size="sm" mb="sm" style={{ fontWeight: 900, }}>
                 {ui.article?.description ?? "Here's the news description."}
             </Text>
             <Stack spacing="sm" p="xs" style={{ backgroundColor: '#d3eaff', borderRadius: 12, }}>
-                <Image radius="md" fit="contain" alt={ui.article?.title} width={100} height={250} src={ui.article?.imageUrl ?? '/dark.png'} />
+                <Image radius="md" fit="contain" alt={ui.article?.title} width={100} height={250} src={ui.article?.imageUrl ?? null} />
                 <Button radius="md" mt="sm" ml="auto" mr="auto" style={{ width: 'fit-content'}}>{ui.article?.ctaText ?? 'Play now'}</Button>
             </Stack>
         </Paper>
