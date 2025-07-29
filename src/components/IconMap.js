697a58ecfd924244c5e391bc7c3068a0fd76359f
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-28 19:32:12 -0500

    Simplified the imports of Tabler Icons

---
+++ src/components/IconMap.js
@@ -0,0 +1,42 @@
+import { IconNews, IconGift, IconAward, IconTarget, IconPencil, IconUpload, IconSkateboard } from '@tabler/icons-react';
+import { IconUser, IconUserEdit, IconUserCircle, IconUserSearch } from '@tabler/icons-react';
+import { IconMoodHappy } from '@tabler/icons-react';
+import { IconSpy, IconRobot, IconAlien } from '@tabler/icons-react';
+import { IconFaceId, IconFaceIdError } from '@tabler/icons-react';
+import { IconStar, IconLock } from '@tabler/icons-react';
+
+const iconMap = {
+  News: IconNews,
+  Gift: IconGift,
+  Jake: IconUser,
+  Tricky: IconMoodHappy,
+  Robo: IconRobot,
+  Yutani: IconSpy,
+  Alien: IconAlien,
+  Tagbot: IconFaceId,
+  Lucia: IconUserSearch,
+  Fresh: IconFaceIdError,
+  Frank: IconUserEdit,
+  Zoe: IconUserCircle,
+  King: IconStar,
+  Locked: IconLock,
+  Jack: IconUser,
+  Trick: IconMoodHappy,
+  Rob: IconRobot,
+  Yutan: IconSpy,
+  Alie: IconAlien,
+  Tagbo: IconFaceId,
+  Luci: IconUserSearch,
+  Fres: IconFaceIdError,
+  Fran: IconUserEdit,
+  Zo: IconUserCircle,
+  Kin: IconStar,
+  Lock: IconLock,
+  Award: IconAward,
+  Target: IconTarget,
+  Pencil: IconPencil,
+  Upload: IconUpload,
+  Skateboard: IconSkateboard,
+};
+
+export default iconMap;
