880b01ffd4831c5371064bb86e0e2335e3995926
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-28 19:31:42 -0500

    Reformatted for consistency

--- src/components/SideBarButton.js
+++ src/components/SideBarButton.js
@@ -1,25 +1,17 @@
 import { Paper, ActionIcon, Badge } from '@mantine/core';
 
-export default function SidebarButton({ icon, badge, action }) {
+export default function SidebarButton({ icon, badge, action = () => {} }) {
     return (
-    <Paper
-      shadow="sm"
-      radius="lg"
-      withBorder
-      style={{ position: 'relative', width: 50, height: 50 }}
-      onClick={action}
-    >
+    <Paper shadow="sm" radius="lg" withBorder onClick={action} style={{
+		position: 'relative', width: 50, height: 50
+	}}>
         {badge && (
-            <Badge
-            color="red"
-            variant="filled"
-            size="xs"
-            style={{ position: 'absolute', top: -5, right: -5 }}
-            >{badge}
+            <Badge color="red" variant="filled" size="xs"
+            style={{ position: 'absolute', top: -5, right: -5 }}>
+				{badge}
             </Badge>
         )}
-        <ActionIcon variant="transparent" size="xl" style={{ width: '100%', height: '100%' }}>{icon}
-        </ActionIcon>
+    	<ActionIcon variant="transparent" size="xl" w='100%' h='100%'>{icon}</ActionIcon>
     </Paper>
   );
 }
