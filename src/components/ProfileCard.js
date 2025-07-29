58d055686c664867885b4c7060064c03bce9e999
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-07-28 19:33:37 -0500

    ProfileCard was refactored in order to sync with the backend and enable updates

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,8 +1,9 @@
 import { Paper, Text, Avatar, Group, Stack, Grid, Center, Divider, Modal, Box, ActionIcon } from '@mantine/core';
-import { IconPencil, IconUpload, IconTarget, IconSkateboard } from '@tabler/icons-react';
+import { IconPencil, IconUpload, IconTarget, IconSkateboard, } from '@tabler/icons-react';
 import { IconAward, IconMapPin } from '@tabler/icons-react';
 import { useState } from 'react';
 import EditProfile from './EditProfile';
+import iconMap from './IconMap';
 
 function ProfileCardTool({icon, action}) {
 	return (
@@ -12,18 +13,16 @@ function ProfileCardTool({icon, action}) {
 
 function Medal({ image, size = 32, frameColor = '#a2c4ff' }) {
     return (
-    <Box style={{
-        width: size,
-        height: size,
-        borderRadius: '50%',
-        overflow: 'hidden',
-        border: `4px solid ${frameColor}`,
-        boxShadow: `0 0 8px ${frameColor}`,
-        transform: 'scale(0.85)',
-        backgroundColor: 'white',
-    }}
-    >
-        {image}
+    <Box bdrs='50%' style={{
+        width: size, height: size, overflow: 'hidden', transform: 'scale(0.85)', backgroundColor: 'white',
+        border: `4px solid ${frameColor}`, boxShadow: `0 0 8px ${frameColor}`,
+    }}>
+        {typeof image === 'string' && iconMap[image] ? 
+            (() => {
+                const IconComponent = iconMap[image];
+                return IconComponent ? <IconComponent size={24} /> : null;
+            })()
+            : image}
     </Box>
   );
 }
@@ -66,11 +65,9 @@ const medals = [
     {medal: <Medal image={<IconUpload />} />, label: 'Upload'},
 ]
 
-function Profile({ ui }) {
+function Profile({ ui = {}, data = {} }) {
   return (
-    <Paper shadow="lg" radius="xl" p="md" withBorder style={{
-        width: '100%',
-        margin: 'auto',
+    <Paper shadow="lg" radius="xl" w='100%' m='auto' p="md" withBorder style={{
         background: 'linear-gradient(to bottom, #d2ebff, #a2c4ff)',
         border: '3px solid #1a629cff'
     }}
@@ -81,29 +78,33 @@ function Profile({ ui }) {
                     <ProfileCardTool icon={<IconPencil size={20}/>} action={() => {ui.UpdateProfileName(true)}} />
                     <ProfileCardTool icon={<IconUpload size={20}/>} action={() => {ui.ShareProfile(true)}} />
                 </Stack>
-                <Avatar src="/avatar.png" size="lg" radius="xl" />
+                <Avatar src="/avatar.png" size="lg" radius="xl">
+                {typeof data.avatar === 'string' && iconMap[data.avatar] ? 
+                    (() => {
+                        const IconComponent = iconMap[data.avatar];
+                        return IconComponent ? <IconComponent size={24} /> : null;
+                    })()
+                    : data.avatar
+                }
+                </Avatar>
                 <Stack spacing={0} gap={0}>
-                    <Text fw={700} size="lg">Oracle</Text>
-                    <Text size="sm" c="dimmed">The Aesir</Text>
+                    <Text fw={700} size="lg">{data.name}</Text>
+                    <Text size="sm" c="dimmed">{data.team}</Text>
                 </Stack>
             </Group>
         </Group>
 
-        <ProfileCardBar title={'Bio'} value={{'text': 'For Fortune and Glory...', 'size': '0.75rem', 'style': 'italic'}} />
+        <ProfileCardBar title={'Bio'} value={{'text': data.bio, 'size': '0.75rem', 'style': 'italic'}} />
 		<Grid gutter="sm" mb="md" mt='md'>
 
 			{/* Character Placeholder */}
 			<Grid.Col span={5}>
 				<Center style={{ height: '100%' }}>
-					<Box
-        			style={{
-          			width: 100,
-          			height: 140,
-          			borderRadius: 12,
-          			background: 'linear-gradient(to bottom, #ffe68c, #ffd34d)',
-          			boxShadow: '0 4px 10px rgba(0,0,0,0.2)',
-        			}}
-      				>
+					<Box style={{
+          			    width: 100, height: 140, borderRadius: 12,
+          			    background: 'linear-gradient(to bottom, #ffe68c, #ffd34d)',
+                        boxShadow: '0 4px 10px rgba(0,0,0,0.2)',
+        			}}>
 						{/* add a <Character3D /> here */}
       				</Box>
     			</Center>
@@ -112,10 +113,18 @@ function Profile({ ui }) {
             {/* Stats */}
   		    <Grid.Col span={7}>
     		    <Stack spacing="xs">
-      			    <ProfileCardBar title="destinations" value={{ text: '7/459', size: '1rem' }} icon={<IconTarget size={18} />} />
-      			    <ProfileCardBar title="trophies" value={{ text: '7/446', size: '1rem' }} icon={<IconSkateboard size={18} />} />
-      			    <ProfileCardBar title="achievements" value={{ text: '3/27', size: '1rem' }} icon={<IconAward size={18} />} />
-      			    <ProfileCardBar title="titles" value={{ text: '21/81', size: '1rem' }} icon={<IconMapPin size={18} />} />
+      			    <ProfileCardBar title="destinations" value={{
+                        text: `${data.destinations[0]}/${data.destinations[1]}`, size: '1rem'}}
+                        icon={<IconTarget size={18} />} />
+      			    <ProfileCardBar title="trophies" value={{
+                        text: `${data.trophies[0]}/${data.trophies[1]}`, size: '1rem' }}
+                        icon={<IconSkateboard size={18} />} />
+      			    <ProfileCardBar title="achievements" value={{
+                        text: `${data.achievements[0]}/${data.achievements[1]}`, size: '1rem' }}
+                        icon={<IconAward size={18} />} />
+      			    <ProfileCardBar title="titles" value={{
+                        text: `${data.titles[0]}/${data.titles[1]}`, size: '1rem' }}
+                        icon={<IconMapPin size={18} />} />
     		    </Stack>
   		    </Grid.Col>
 	    </Grid>
@@ -125,9 +134,7 @@ function Profile({ ui }) {
         <ProfileCardBar title={'Accolades'}>
             <Box style={{ width: 320 }} ml='auto' mr='auto'>
                 <Group style={{'--group-justify': 'space-evenly'}}>
-                    {medals.map((m, i) => (
-                        <Box key={i}>{m.medal}</Box>
-                    ))}
+                    {data.accolades.map((m, i) => <Box key={i}><Medal image={m} label={m} /></Box>)}
                 </Group>
             </Box>
         </ProfileCardBar>
@@ -135,26 +142,23 @@ function Profile({ ui }) {
   );
 }
 
-export default function ProfileCard({ ui={} }) {
+export default function ProfileCard({ ui = {}, data = {}, avatars = [] }) {
     const [updateProfileName, UpdateProfileName] = useState(false)
     const [shareProfile, ShareProfile] = useState(false)
 
-    const uiState = {
+    const localUI = {
         ...ui,
-        updateProfileName,
-		UpdateProfileName,
-		shareProfile,
-		ShareProfile,
+        updateProfileName, UpdateProfileName,
+		shareProfile, ShareProfile,
     }
+
 	return (
-        <>
-        <Modal radius="xl" padding={0} size="sm" centered opened={ui.profileCardToggle} withCloseButton={false}
-        onClose={() => uiState.ProfileCardToggle(false)}
-	    overlayProps={{ opacity: '0.6' }}
-        >
-		    <Profile ui={uiState} />
-	    </Modal>
-        <EditProfile ui={uiState} />
-        </>
+    <>
+    <Modal radius="xl" padding={0} size="sm" centered opened={ui.admireProfile} withCloseButton={false}
+    onClose={() => ui.AdmireProfile(false)} overlayProps={{ opacity: '0.6' }}>
+		<Profile ui={localUI} data={data}/>
+	</Modal>
+    <EditProfile ui={localUI} data={data} avatars={avatars}/>
+    </>
 	);
 }
