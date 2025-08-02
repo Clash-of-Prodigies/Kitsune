eddcc01e64631bea84f5f36ae94f88715f6a3fff
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-08-02 09:17:45 -0500

    Moved functions from Sidebar.js to here

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,56 @@
-import { Box, Container, Center, Stack } from '@mantine/core';
+import { Box, Container, Center, Stack, Paper, ActionIcon, Badge } from '@mantine/core';
 import { IconNews, IconCalendar, IconTournament, IconHours24, IconSparkles, } from '@tabler/icons-react';
 import { IconHome, IconUser, IconTrophy, IconBarbell, IconSettings, } from '@tabler/icons-react';
-import { useState, useEffect } from 'react';
+import { useState, } from 'react';
 import ResourceBar from '../components/ResourceBar';
 import BottomNav from '../components/BottomNav';
-import SidebarButton from '../components/SideBarButton';
 import ProfileCard from '../components/ProfileCard';
 import NewsCard from '../components/NewsCard';
 import CalendarCard from '../components/CalendarCard';
 import SettingsCard from '../components/SettingsCard';
 
+function LeftSidebarButton({ icon, badge, action = () => {} }) {
+	return (
+	<Paper shadow="sm" radius="lg" withBorder onClick={action} style={{
+		position: 'relative', width: 50, height: 50
+	}}>
+		{badge && (
+			<Badge color="red" variant="filled" size="xs" style={{
+				position: 'absolute', top: -5, right: -5
+			}}>
+				{badge}
+			</Badge>
+		)}
+		<ActionIcon variant="transparent" size="xl" w='100%' h='100%'>{icon}</ActionIcon>
+	</Paper>
+	);
+}
+
+function RightSidebarButton({ icon, badge, action = () => {} }) {
+    return (
+    <Paper shadow="sm" radius="lg" withBorder onClick={action} bdrs={'50%'} style={{
+		position: 'relative', width: 50, height: 50
+	}}>
+		{badge && (
+			<Badge color="red" variant="filled" size="xs" style={{
+				position: 'absolute', top: -5, right: -5
+			}}>
+				{badge}
+            </Badge>
+        )}
+    	<ActionIcon variant="transparent" size="xl" w='100%' h='100%'>{icon}</ActionIcon>
+    </Paper>
+	);
+}
+
+
 function LeftSidebar({ ui = {} }) {
 	return (
     <Stack spacing="sm" pos='absolute' left={10}>
-		<SidebarButton icon={<IconUser />} action={() => ui.AdmireProfile(true)} />
-		<SidebarButton icon={<IconSettings />} action={() => ui.OpenSettings(true)}/>
-		<SidebarButton icon={<IconNews />} action={() => ui.ReadNews(true)}/>
-		<SidebarButton icon={<IconCalendar />} action={() => ui.ViewCalendar(true)} />
+		<LeftSidebarButton icon={<IconUser />} action={() => ui.AdmireProfile(true)} />
+		<LeftSidebarButton icon={<IconNews />} action={() => ui.ReadNews(true)} />
+		<LeftSidebarButton icon={<IconCalendar />} action={() => ui.ViewCalendar(true)} />
+		<LeftSidebarButton icon={<IconSettings />} action={() => ui.OpenSettings(true)} />
     </Stack>
 	);
 }
@@ -24,12 +58,12 @@ function LeftSidebar({ ui = {} }) {
 function RightSidebar({ ui = {} }) {
 	return (
     <Stack spacing="sm" pos='absolute' right={10}>
-		<SidebarButton icon={<IconTrophy />} />
-		<SidebarButton icon={<IconHome />} />
-		<SidebarButton icon={<IconTournament />} />
-		<SidebarButton icon={<IconHours24 />}  badge={1}/>
-		<SidebarButton icon={<IconBarbell />} />
-		<SidebarButton icon={<IconSparkles />} />
+		<RightSidebarButton icon={<IconTrophy />} />
+		<RightSidebarButton icon={<IconHome />} />
+		<RightSidebarButton icon={<IconTournament />} />
+		<RightSidebarButton icon={<IconHours24 />}  badge={1} />
+		<RightSidebarButton icon={<IconBarbell />} />
+		<RightSidebarButton icon={<IconSparkles />} />
     </Stack>
   );
 }
@@ -55,20 +89,11 @@ function Layout({ children, ui }) {
 	);
 }
 
-export default function Home({ ui = {}, dossier = {} }) {
-	const [broadcast, Listen] = useState({});
+export default function Home({ ui = {}, dossier = {}, broadcast = {} }) {
 	const [admireProfile, AdmireProfile] = useState(false);
 	const [readNews, ReadNews] = useState(false);
 	const [viewCalendar, ViewCalendar] = useState(false);
 	const [openSettings, OpenSettings] = useState(false);
-
-  	useEffect(() => {
-		Promise.all([
-			fetch('http://localhost:5000/broadcast').then(res => res.json()),
-		])
-		.then(([announcements,]) =>  [announcements,])
-		.then((res) => {Listen(res[0]); /*console.log(res); */})
-	}, []);
 
 	const globalUI = {
 		...ui,
@@ -84,7 +109,7 @@ export default function Home({ ui = {}, dossier = {} }) {
 			<ProfileCard ui={globalUI} data={dossier.info} avatars={broadcast.avatars} />
 			<NewsCard ui={globalUI} articles={broadcast.news} />
 			<CalendarCard ui={globalUI} />
-			<SettingsCard ui={globalUI} data={dossier.info} />
+			<SettingsCard ui={globalUI} data={dossier.info} playlists={broadcast.playlists} />
       	</Center>
 	</Layout>
 	);
