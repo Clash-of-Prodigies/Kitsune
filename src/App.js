e4c471ca89b4161e05935f2a600fb9a566081057
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-08-02 09:15:08 -0500

    Refactored for audio capabilities... which are awesome

--- src/App.js
+++ src/App.js
@@ -10,20 +10,28 @@ import React from 'react';
 
 export default function App() {
 	const [dossier, Mutate] = useState({});
+	const [broadcast, Listen] = useState({});
 	const [loading, Load] = useState(true);
 	const [error, Spit] = useState(null);
+	const [playlist, Stream] = useState([])
 	const musicPlayer = useRef(null);
 
 	useEffect(() => {
 		if (!loading) return;
 		Promise.all([
 			fetch('http://localhost:5000/data').then(res => res.json()),
+			fetch('http://localhost:5000/broadcast').then(res => res.json()),
 		])
-		.then(([user,]) =>  [user,])
-		.then((res) => {Mutate(res[0]); console.log(res[0]);})
+		.then(([user, announcements, ]) => [user, announcements, ])
+		.then((res) => {Mutate(res[0]); Listen(res[1]); Stream(res[1].playlists[res[0].info.playlist]); /*console.log(res);*/})
 		.catch((err) => Spit(err))
 		.finally(() => Load(false));
 	}, [loading]);
+
+	useEffect(() => {
+		if (!musicPlayer.current) return;
+		musicPlayer.current.src = playlist ? `http://localhost:5000/media/${playlist[0]}.mp3` : null ;
+	}, [playlist])
 
 	if (loading) return null;
 	// if (loading) return <p>Loading data...</p>;
@@ -36,13 +44,17 @@ export default function App() {
 
 	return (
 	<HashRouter>
-		<audio ref={musicPlayer} muted autoPlay loop style={{ display: 'none' }}
-		src={dossier?.info?.playlist ? `http://localhost:5000/media/${dossier.info.playlist}.mp3` : ''} />
+		<audio ref={musicPlayer} muted autoPlay style={{ display: 'none' }}
+		onEnded={() => {
+			const tempPlaylist = [...playlist];
+			tempPlaylist.push(tempPlaylist.shift());
+			Stream(tempPlaylist);
+		}}/>
 		<Routes>
 			<Route index path="/" element={
 				<React.StrictMode>
 					<MantineProvider withGlobalStyles withNormalizeCSS>
-						<Home ui={globalUI} dossier={dossier}/>
+						<Home ui={globalUI} dossier={dossier} broadcast={broadcast} />
 					</MantineProvider>
 				</React.StrictMode>
 			} />
