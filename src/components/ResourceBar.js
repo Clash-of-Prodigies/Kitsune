ca850ecc9c3b386be8672e24d7600fd6a3c9944d
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-08-03 17:16:16 -0500

    Refactored ResourceBar component for improved readability and consistency

--- src/components/ResourceBar.js
+++ src/components/ResourceBar.js
@@ -1,4 +1,3 @@
-// src/components/ResourceBar.jsx
 import { Group, Paper, Text, ActionIcon } from '@mantine/core';
 import { IconKey, IconCoin, IconFlame, IconStar, IconPlus } from '@tabler/icons-react';
 
@@ -10,29 +9,19 @@ const resources = [
 ];
 
 export default function ResourceBar() {
-  return (
-    <Paper
-      p="xs"
-      radius="lg"
-      shadow="sm"
-      withBorder
-      style={{
-        backgroundColor: '#fdf2c2',
-        borderColor: '#ffc107',
-        overflowX: 'auto',
-      }}
-    >
-      <Group spacing="md" justify='space-evenly'>
-        {resources.map((res, index) => (
-          <Group spacing={4} key={index}>
-            <res.icon width={24} height={24} alt="icon" />
-            <Text fw={700} size="sm">{res.amount.toLocaleString()}</Text>
-            <ActionIcon variant="light" color="yellow" size="sm">
-              <IconPlus size={14} />
-            </ActionIcon>
-          </Group>
-        ))}
-      </Group>
+	return (
+	<Paper p="xs" radius="lg" shadow="sm" withBorder style={{
+		backgroundColor: '#fdf2c2', borderColor: '#ffc107', overflowX: 'auto',
+	}}>
+		<Group spacing="md" justify='space-evenly'>
+			{resources.map((res, index) => (
+				<Group spacing={4} key={index}>
+					<res.icon width={24} height={24} alt="icon" />
+					<Text fw={700} size="sm">{res.amount.toLocaleString()}</Text>
+					<ActionIcon variant="light" color="yellow" size="sm"><IconPlus size={14} /></ActionIcon>
+        		</Group>
+        	))}
+      	</Group>
     </Paper>
-  );
+	);
 }
