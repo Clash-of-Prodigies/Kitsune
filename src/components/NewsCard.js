3053eca65da8ec6e951dfdca40a83a2b9182854f
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-08-10 04:18:58 -0500

    Add Article component to NewsCard for improved article display

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,8 +1,29 @@
-import { Modal, Stack, Paper, Text, Box, Group, Badge, ScrollArea, Center, Title } from '@mantine/core';
+import { Modal, Stack, Paper, Text, Box, Group, Badge, ScrollArea, Center, Title, Image, Button } from '@mantine/core';
 import { IconClockHour4, } from '@tabler/icons-react';
 import { useState, } from 'react';
-import Article from './Article';
 import IconOrImage from './IconMap';
+
+function Article({ ui = {} }) {
+    return (
+    <Modal centered withCloseButton={false} radius="xl" padding={0} overlayProps={{ opacity: 0.6 }} size="sm"
+    opened={ui.article !== null} onClose={() => ui.ReadArticle(null)
+    }>
+        <Paper p="xs" radius="xs" style={{ backgroundColor: '#96cbfdff', width: '100%', }}>
+            <Text align="center" c='#f8fdffff' size="xl" mb="sm" ff='sans-serif' fw={900} style={{
+                textShadow: '1px 1px #3772ff',
+            }}>{ui.article?.title ?? 'News Title'}</Text>
+
+            <Text align="center" c='#080aa0ff' size="sm" mb="sm" style={{ fontWeight: 900, }}>
+                {ui.article?.description ?? "Here's the news description."}
+            </Text>
+            <Stack spacing="sm" p="xs" style={{ backgroundColor: '#d3eaff', borderRadius: 12, }}>
+                <Image radius="md" fit="contain" alt={ui.article?.title} width={100} height={250} src={ui.article?.imageUrl ?? null} />
+                <Button radius="md" mt="sm" ml="auto" mr="auto" style={{ width: 'fit-content'}}>{ui.article?.ctaText ?? 'Play now'}</Button>
+            </Stack>
+        </Paper>
+    </Modal>
+    );
+}
 
 function NewsArticle({ui = {}, article={}}) {
     return (
