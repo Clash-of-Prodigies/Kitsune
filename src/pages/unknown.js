45d5f18aee5b3c8df364befaedf5bd54bfd41d27
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-08-10 23:30:55 -0500

    Refactor Unknown component: replace div with Box for improved structure and consistency

--- src/pages/unknown.js
+++ src/pages/unknown.js
@@ -1,24 +1,24 @@
-import { Button, Container, Image, SimpleGrid, Text, Title } from '@mantine/core';
+import { Button, Container, Image, SimpleGrid, Text, Title, Box } from '@mantine/core';
 import image from '../media/unknown.svg';
 import classes from '../styles/unknown.module.css';
 
 export default function Unknown() {
-  return (
+	return (
     <Container className={classes.root}>
-      <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
-        <Image src={image} className={classes.mobileImage} />
-        <div>
-          <Title className={classes.title}>Something is not right...</Title>
-          <Text c="dimmed" size="lg">
-            Page you are trying to open does not exist. You may have mistyped the address, or the
-            page has been moved to another URL. If you think this is an error contact support.
-          </Text>
-          <Button variant="outline" size="md" mt="xl" className={classes.control}>
-            Get back to home page
-          </Button>
-        </div>
-        <Image src={image} className={classes.desktopImage} />
-      </SimpleGrid>
+		<SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
+			<Image src={image} className={classes.mobileImage} />
+			<Box>
+				<Title className={classes.title}>Something is not right...</Title>
+				<Text c="dimmed" size="lg">
+					Page you are trying to open does not exist. You may have mistyped the address, or the
+					page has been moved to another URL. If you think this is an error contact support.
+          		</Text>
+          		<Button variant="outline" size="md" mt="xl" className={classes.control}>
+					Get back to home page
+          		</Button>
+        	</Box>
+        	<Image src={image} className={classes.desktopImage} />
+      	</SimpleGrid>
     </Container>
-  );
+	);
 }
