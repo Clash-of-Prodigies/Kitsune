f10c056be63a6c0436cc363044a2a32bbdc47678
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-12-30 18:05:55 -0600

    fix: update main.jsx to improve routing and enhance user experience

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App, {Countdown} from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
         <MantineProvider withGlobalStyles withNormalizeCSS>
-            <HashRouter basename={import.meta.env.BASE_URL}><App /></HashRouter>
+            <BrowserRouter basename={import.meta.env.BASE_URL}><App /></BrowserRouter>
             <Countdown />
         </MantineProvider>
 </StrictMode>
